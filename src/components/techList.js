f36cbb34f86f5570fda532ff49be3b6d3d68f62c
MelquiasRibeiro <melquias10.mr@gmail.com>
2020-04-18 22:51:53 -0300

    concept: state and imutability

--- src/components/techList.js
+++ src/components/techList.js
@@ -3,21 +3,43 @@ import React, { Component } from 'react';
 
 export default class TechList extends Component {
   state = {
+    newTech:[
+      ''
+    ],
     techs:[
       'Node.js',
       'React.js',
       'React Native'
     ]
   };
+  handleIputChange = e => {
+    this.setState({newTech: e.target.value})
+  }
+
+  handleSubmit= e =>{
+    e.preventDefault();
+
+    this.setState({ techs:[...this.state.techs,this.state.newTech],
+      newTech:''
+    })
+  }
   
   render() {
-    console.log(this.state)
     return(
+      <>
       <ul>
-        <li>node.js</li>
-        <li>React.js</li>
-        <li>React Native</li>
+      {this.state.techs.map(tech=><li key={tech}>{tech}</li>)}
       </ul>
+      <form onSubmit={this.handleSubmit}>
+          <input type="text"
+             onChange={this.handleIputChange} 
+             placeholder="digite aqui uma nova tecnologia"
+             value={this.state.newTech}/>
+          <button type="submit">
+            Enviar
+          </button>
+      </form>
+      </>
     );
   }
 }
